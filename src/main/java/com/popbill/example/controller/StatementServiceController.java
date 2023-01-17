64810717e4a82eecbf193249ac3efd8ccd3bb3d0
seunghwan <lsh@linkhubcorp.com>
2023-01-17 17:06:35 +0900

    emailSubject 주석보충

--- src/main/java/com/popbill/example/controller/StatementServiceController.java
+++ src/main/java/com/popbill/example/controller/StatementServiceController.java
@@ -649,6 +649,7 @@ public class StatementServiceController {
         String memo = "발행메모";
         
         // 전자명세서 발행 안내메일 제목
+        // - 미입력시 팝빌에서 지정한 이메일 제목으로 전송
         String emailSubject = "테스트";
 
         try {
