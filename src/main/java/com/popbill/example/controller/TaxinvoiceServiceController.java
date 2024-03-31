a8b30fd3fd44ffc9b378aa40cc9f2792abb4584e
hyeseon <hyeseon@linkhubcorp.com>
2024-04-01 08:49:19 +0900

    주석 문구 수정

--- src/main/java/com/popbill/example/controller/TaxinvoiceServiceController.java
+++ src/main/java/com/popbill/example/controller/TaxinvoiceServiceController.java
@@ -1857,8 +1857,6 @@ public class TaxinvoiceServiceController {
         // 연동문서 - 세금계산서 작성 시 API를 통해 등록한 문서
         String InterOPYN = null;
 
-        String UserID = "testkorea";
-
         try {
 
             TISearchResult searchResult = taxinvoiceService.Search(CorpNum, mgtKeyType, DType,
@@ -4880,7 +4878,7 @@ public class TaxinvoiceServiceController {
     public String registTaxCert(Model m) {
         /**
          * 전자세금계산서 발행에 필요한 공동인증서를 팝빌 인증서버에 등록합니다.
-         * 공동인증서는 팝빌에서 발급하는 '표준 인증서'와 '범용 인증서', 은행에서 발급하는 '전자세금용 인증서'만 등록이 가능합니다.
+         * 공동인증서는 팝빌에서 발급하는 '표준 인증서', 은행에서 발급하는 '전자세금용 인증서' 또는 '기업범용 인증서'만 등록 가능합니다.
          * 공동인증서 정보는 통신 구간의 보안을 위해 필드 레벨 암호화(FLE)되어 처리됩니다.
          * 통신구간 암호화 키 발급은 파트너 센터(1600-8536)로 문의하여 주시기 바랍니다.
          * 공동인증서 비밀번호의 안전한 관리를 위해 DB 저장시 컬럼 암호화 하여 저장합니다.
@@ -4899,9 +4897,6 @@ public class TaxinvoiceServiceController {
         // 공동인증서 비밀번호
         String CertCipher = "";
 
-        // 팝빌회원 아이디
-        String UserID = "";
-
         try {
             Response response = taxinvoiceService.registTaxCert(CorpNum, CertPublicKey, CertPrivateKey, CertCipher, UserID);
             m.addAttribute("Response", response);
@@ -4917,7 +4912,7 @@ public class TaxinvoiceServiceController {
     public String registTaxCertPFX(Model m) {
         /**
          * 전자세금계산서 발행에 필요한 공동인증서를 팝빌 인증서버에 등록합니다.
-         * 공동인증서는 팝빌에서 발급하는 '표준 인증서'와 '범용 인증서', 은행에서 발급하는 '전자세금용 인증서'만 등록이 가능합니다.
+         * 공동인증서는 팝빌에서 발급하는 '표준 인증서', 은행에서 발급하는 '전자세금용 인증서' 또는 '기업범용 인증서'만 등록 가능합니다.
          * 공동인증서 정보는 통신 구간의 보안을 위해 필드 레벨 암호화(FLE)되어 처리됩니다.
          * 통신구간 암호화 키 발급은 파트너 센터(1600-8536)로 문의하여 주시기 바랍니다.
          * 공동인증서 비밀번호의 안전한 관리를 위해 DB 저장시 컬럼 암호화 하여 저장합니다.
@@ -4932,9 +4927,6 @@ public class TaxinvoiceServiceController {
 
         // 공동인증서 PXF 파일 비밀번호
         String Password = "";
-
-        // 팝빌회원 아이디
-        String UserID = "testkorea";
 
         try {
             Response response = taxinvoiceService.registTaxCertPFX(CorpNum, PFX, Password, UserID);
