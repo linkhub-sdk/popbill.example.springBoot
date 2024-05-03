73ae86a711d3a4ee96cc0ccbdc064a8f17ea33ec
hyeseon <hyeseon@linkhubcorp.com>
2024-05-03 09:55:47 +0900

    fax - getPreviewURL 국문명 변경

--- src/main/java/com/popbill/example/controller/FaxServiceController.java
+++ src/main/java/com/popbill/example/controller/FaxServiceController.java
@@ -794,7 +794,7 @@ public class FaxServiceController {
     @RequestMapping(value = "getPreviewURL", method = RequestMethod.GET)
     public String getPrevewURL(Model m) {
         /**
-         * 팩스 미리보기 팝업 URL을 반환하며, 팩스전송을 위한 TIF 포맷 변환 완료 후 호출 할 수 있습니다.
+         * 팩스 변환결과 확인 팝업 URL을 반환하며, 팩스전송을 위한 TIF 포맷 변환 완료 후 호출 할 수 있습니다.
          * - 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
          * - https://developers.popbill.com/reference/fax/java/api/info#GetPreviewURL
          */
