5305759228692e3b252fe5d85f1a0c25dcaefec7
gyuzero <gyuzero@linkhubcorp.com>
2025-07-07 16:00:07 +0900

    예금주 식별정보 -> 예금주 실명번호, 메모 -> 거래내역 메모

--- src/main/java/com/popbill/example/controller/EasyFinBankServiceController.java
+++ src/main/java/com/popbill/example/controller/EasyFinBankServiceController.java
@@ -66,7 +66,7 @@ public class EasyFinBankServiceController {
         // 계좌유형, "법인" / "개인" 중 택 1
         bankInfo.setAccountType("");
 
-        // 예금주 식별정보 ('-' 제외)
+        // 실명번호 ('-' 제외)
         // 계좌유형이 "법인"인 경우 : 사업자번호(10자리)
         // 계좌유형이 "개인"인 경우 : 예금주 생년월일 (6자리-YYMMDD)
         bankInfo.setIdentityNumber("");
@@ -448,7 +448,7 @@ public class EasyFinBankServiceController {
         // └ 거래내역 조회(Seach API) 함수의 반환값인 EasyFinBankSearchDetail 의 tid를 통해 확인 가능
         String TID = "02110251200000000420211231000001";
 
-        // 메모
+        // 거래내역 메모
         String Memo = "Memo테스트";
 
         try {
